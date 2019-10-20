d7b1327dfa5d145634931712ed5d0ad293858dd6
Bajek <bartek.urbaniec@gmail.com>
2019-10-20 23:04:40 +0200

    Lombok :)

--- src/main/java/pl/ubytes/unotifier/UNotifierApplication.java
+++ src/main/java/pl/ubytes/unotifier/UNotifierApplication.java
@@ -1,15 +1,22 @@
 package pl.ubytes.unotifier;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@Slf4j
 public class UNotifierApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UNotifierApplication.class, args);
+		try {
+			SpringApplication.run(UNotifierApplication.class, args);
+		} catch (Exception e) {
+			log.error("Cannot start application", e);
+			System.exit(-1);
+		}
 	}
 
 }
